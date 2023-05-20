fe38b58968efe1ada59243557e0fa759f36fc5fd
hridoyfahad30 <hridoyfahad30@gmail.com>
2023-05-20 18:31:36 +0600

    Setup JWT

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-require('dotenv').config();
-const { MongoClient, ServerApiVersion, ObjectId, CancellationToken } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+app.use(cors());
+app.use(express.json());
+require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
@@ -22,6 +25,23 @@ const client = new MongoClient(uri, {
   }
 });
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error: true, message: 'unauthorized access'});
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.USER_ACCESS_TOKEN, (error, decoded) => {
+    if(error){
+      return res.status(401).send({error: true, message: 'unauthorized access'})
+    }
+    req.decoded = decoded;
+    next()
+  })
+}
+
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -29,6 +49,14 @@ async function run() {
     // Send a ping to confirm a successful connection
 
     const toyCollection = client.db('toyDB').collection('toys');
+
+
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.USER_ACCESS_TOKEN, {expiresIn: '1h'});
+      res.send({token});
+    })
+
 
     app.get('/toys', async (req, res) => {
 
@@ -41,8 +69,32 @@ async function run() {
       const query = {_id: new ObjectId(id)}
       const result = await toyCollection.findOne(query)
       res.send(result)
+    });
+
+    // app.get('/myToys', async (req, res) => {
+    //   let query = {};
+    //   if(req.query?.email){
+    //     query = {email: req.query.email}
+    //   }
+    //   const result = await bookingCollection.find(query).toArray();
+    //   res.send(result)
+    // })
+
+    app.get('/myToys', verifyJWT, async(req, res) => {
+      const decoded = req.decoded;
+      console.log(req.query.email);
+
+      if(decoded.email !== req.query.email){
+        return res.status(403).send({error: true, message: 'forbidden access'})
+      }
+      let query = {};
+      if(req.query?.email){
+        query = {sellerEmail: req.query.email}
+      }
+      const result = await toyCollection.find(query).toArray();
+      res.send(result)
       console.log(result);
-    })
+    });
 
 
 
